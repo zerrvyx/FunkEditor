f100d5e74a9d81222235c0cf48fbc0f175218572
Stilic <lateman-jpeg@outlook.fr>
2022-06-06 03:51:34 +0200

    i likz

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,19 @@
 package;
 
+import flixel.FlxState;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	static var initialState:Class<FlxState> = PlayState;
+	static var framerate:Int = 60;
+	// I HATE FLIXEL JUMPSCARE!!2!1!
+	static var skipSplash:Bool = true;
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, initialState, framerate, framerate, skipSplash));
 	}
 }
